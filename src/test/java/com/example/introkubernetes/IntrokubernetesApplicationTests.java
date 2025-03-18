c944a38540d30ac6b194f71b8722f6de448dc30a
RajshriSkillup <rajashreep@skillup.online>
2025-03-18 07:43:07 -0400

    git push origin main
    Initial commit

---
+++ src/test/java/com/example/introkubernetes/IntrokubernetesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.introkubernetes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntrokubernetesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
